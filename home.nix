7939784b4eee81130816b4d70d515445ac43175a
arthur <arthurfleck8@proton.me>
2025-10-06 01:56:04 +0200

    add more pkgs

--- home.nix
+++ home.nix
@@ -15,11 +15,23 @@
 #  home.file.".config/qtile".source = ./config/qtile;
 
   home.packages = with pkgs; [
-	ripgrep
+        emacs
+	      ripgrep
         stow
-        fish
         delta
+        tokei
+	      starship
+        rbenv
+        zoxide
+        ncdu
+        htop
+        i3status-rust
+        arandr
   ];
+
+  # this overwrote the config in ~/dotfiles (make backup like we set it up though)
+  # using existing config file for now 
+  # programs.fish.enable = true;
 }
 
 
