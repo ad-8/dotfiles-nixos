0c812d53a93ea22301143a9d509604ce824e594e
arthur <arthurfleck8@proton.me>
2025-10-06 11:41:01 +0200

    add git config to home-manager

--- home.nix
+++ home.nix
@@ -3,7 +3,38 @@
 {
   home.username = "ax";
   home.homeDirectory = "/home/ax";
-  programs.git.enable = true;
+  programs.git = {
+    enable = true;
+    extraConfig = {
+      user = {
+        name = "arthur";
+        email = "arthurfleck8@proton.me";
+      };
+      core = {
+        editor = "vim";
+        pager = "delta";
+      };
+      interactive = {
+        diffFilter = "delta --color-only";
+      };
+      delta = {
+        navigate = true;    # use n and N to move between diff sections
+        light = false;      # set to true if you're in a terminal w/ a light background color (e.g. the default macOS     terminal)
+        line-numbers = true;
+        side-by-side = true;
+      };
+      merge = {
+        conflictstyle = "diff3";
+      };
+      diff = {
+        colorMoved = "default";
+      };
+
+
+      credential."https://github.com".helper = "!${pkgs.gh}/bin/gh auth git-credential";
+      credential."https://gist.github.com".helper = "!${pkgs.gh}/bin/gh auth git-credential";
+    };
+  }; 
   home.stateVersion = "25.05";
   programs.bash = {
 	  enable = true;
