65e9ba680fa69cc9c135e988ffed4b8617c85cf8
arthur <arthurfleck8@proton.me>
2025-10-06 11:59:02 +0200

    refactor

--- home.nix
+++ home.nix
@@ -1,8 +1,33 @@
 { config, pkgs, ... }:
 
 {
+  home.stateVersion = "25.05";
   home.username = "ax";
   home.homeDirectory = "/home/ax";
+  home.packages = with pkgs; [
+    arandr
+    bat 
+    delta
+    emacs
+    eza
+    gh
+    htop
+    i3status-rust
+    ncdu
+    rbenv
+    ripgrep
+    starship
+    stow
+    tokei
+    zoxide
+  ];
+
+  programs.bash = {
+	  enable = true;
+    shellAliases = {
+      btw = "echo 'home-manager seems to be working :)'";
+    };
+  };
   programs.git = {
     enable = true;
     extraConfig = {
@@ -29,43 +54,13 @@
       diff = {
         colorMoved = "default";
       };
-
-
       credential."https://github.com".helper = "!${pkgs.gh}/bin/gh auth git-credential";
       credential."https://gist.github.com".helper = "!${pkgs.gh}/bin/gh auth git-credential";
     };
   }; 
-  home.stateVersion = "25.05";
-  programs.bash = {
-	  enable = true;
-    shellAliases = {
-      btw = "echo 'home-manager seems to be working :)'";
-    };
-  };
 
-  #  home.file.".config/qtile".source = ./config/qtile;
-
-  home.packages = with pkgs; [
-    arandr
-    bat 
-    delta
-    emacs
-    eza
-    gh
-    htop
-    i3status-rust
-    ncdu
-    rbenv
-    ripgrep
-    starship
-    stow
-    tokei
-    zoxide
-  ];
-
-  # this overwrote the config in ~/dotfiles (make backup like we set it up though)
-  # using existing config file for now 
+  # this overwrote the config in ~/dotfiles (made backup like we set it up though)
+  # -> using existing config file for now 
   # programs.fish.enable = true;
 }
-
 
