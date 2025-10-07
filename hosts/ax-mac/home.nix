645df521938ddfcc1312ad415964ba292e2a54c5
arthur <arthurfleck8@proton.me>
2025-10-07 15:14:09 +0200

    refactor

--- hosts/ax-mac/home.nix
+++ hosts/ax-mac/home.nix
@@ -3,6 +3,7 @@
 {
 
   imports = [
+    ../../home-manager/git.nix
     ../../home-manager/gtk.nix
   ];
 
@@ -77,42 +78,6 @@
       btw = "echo 'home-manager seems to be working :)'";
     };
   };
-  programs.git = {
-    enable = true;
-    extraConfig = {
-      user = {
-        name = "arthur";
-        email = "arthurfleck8@proton.me";
-      };
-      init = {
-        defaultBranch = "main";
-      };
-      # -------------------------------------------------------------------------------------------------------------------------
-      # settings for [delta](https://github.com/dandavison/delta)
-      core = {
-        editor = "vim";
-        pager = "delta";
-      };
-      interactive = {
-        diffFilter = "delta --color-only";
-      };
-      delta = {
-        navigate = true;    # use n and N to move between diff sections
-        light = false;      # set to true if you're in a terminal w/ a light background color 
-        line-numbers = true;
-        side-by-side = true;
-      };
-      merge = {
-        conflictstyle = "diff3";
-      };
-      diff = {
-        colorMoved = "default";
-      };
-      # -------------------------------------------------------------------------------------------------------------------------
-      credential."https://github.com".helper = "!${pkgs.gh}/bin/gh auth git-credential";
-      credential."https://gist.github.com".helper = "!${pkgs.gh}/bin/gh auth git-credential";
-    };
-  }; 
 
   # this overwrote the config in ~/dotfiles (made backup like we set it up though)
   # -> using existing config file for now 
