4e15ab0b16dbf2f89ffede8dd455ecad3aedb1bb
arthur <arthurfleck8@proton.me>
2025-10-08 12:22:09 +0200

    add pkg for markdown-preview

--- hosts/ax-mac/home.nix
+++ hosts/ax-mac/home.nix
@@ -31,6 +31,7 @@ in
     # other
     gcr # Provides org.gnome.keyring.SystemPrompter
     libqalculate # provides qalc for rofi-calc
+    multimarkdown # to enable markdown-preview in doom emacs
   ] ++ pkgs_base.ps ++ pkgs_gui.ps ++ pkgs_dev.ps ++ pkgs_wm.ps;
 
   programs.bash = {
