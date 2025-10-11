a80797d8c249707cbacdc7e2f42234a48b096483
arthur <arthurfleck8@proton.me>
2025-10-11 17:35:16 +0200

    improve VM integration

--- hosts/ax-vm/configuration.nix
+++ hosts/ax-vm/configuration.nix
@@ -14,7 +14,8 @@
     # boot.initrd.luks.devices."luks-f43e8971-1fb7-4d8c-be86-c8162a78d104".device = "/dev/disk/by-uuid/f43e8971-1fb7-4d8c-be86-c8162a78d104";
     networking.hostName = "ax-vm"; # Define your hostname.
 
-    services.spice-vdagentd.enable = true;
+    services.qemuGuest.enable = true;
+    services.spice-vdagentd.enable = true;  # enable copy and paste between host and guest
 
     services.pulseaudio.enable = false;
     security.rtkit.enable = true;
