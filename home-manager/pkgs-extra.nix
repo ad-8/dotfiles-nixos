f65d39556900b4494e9580baffff2084af35c71f
arthur <arthurfleck8@proton.me>
2025-10-17 12:42:54 +0200

    add pkgs

--- home-manager/pkgs-extra.nix
+++ home-manager/pkgs-extra.nix
@@ -8,11 +8,14 @@
   config = lib.mkIf config.pkgsExtra.enable {
     home.packages = with pkgs; [
       imagemagick
+      inxi # Full featured CLI system information tool
       libqalculate # provides qalc for rofi-calc
-      lm_sensors # provides sensors to show cpu temp etc.
+      lm_sensors # provides `sensors` cmd to show cpu temp etc.
+      pciutils # lspci and more
       pdftk
       smartmontools
       unzip # no zip, use tar xD
+      usbutils # lsusb and more
     ];
   };
 }
