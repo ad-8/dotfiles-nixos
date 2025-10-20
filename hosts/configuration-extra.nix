32df2f62a89c42acffac72c4ddc3c78c2a4924ab
arthur <arthurfleck8@proton.me>
2025-10-20 12:02:08 +0200

    add ffmpeg

--- hosts/configuration-extra.nix
+++ hosts/configuration-extra.nix
@@ -72,6 +72,7 @@
       #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       #  wget
       clamav
+      ffmpeg-full
     ];
 
     services.clamav.daemon.enable = true;
