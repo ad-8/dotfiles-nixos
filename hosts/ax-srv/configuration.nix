f1c39dc4f7dedb4115ebcb197f847a93f1dbe3be
arthur <arthurfleck8@proton.me>
2025-11-10 09:00:25 +0100

    work on server config

--- hosts/ax-srv/configuration.nix
+++ hosts/ax-srv/configuration.nix
@@ -28,7 +28,7 @@
 
     # The firewall is enabled by default on NixOS. Still, explicitly ensure it is enabled
     networking.firewall.enable = true;
-    # Open ports in the firewall.
+    # Open ports in the firewall: [immich, linkding, shiori]
     networking.firewall.allowedTCPPorts = [ 2283 9090 9091 ];
     # networking.firewall.allowedUDPPorts = [ ... ];
 
@@ -54,6 +54,13 @@
       ripgrep
       fzf
     ];
+
+
+    # TODO why bother with separating pkgs?
+    users.users.ax = {
+      packages = with pkgs; [
+      ];
+    };
 
 
     system.stateVersion = "25.05"; # Did you read the comment?
