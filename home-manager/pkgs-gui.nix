bfcaf8d82883a05625f38099d03353b33a432b1a
arthur <arthurfleck8@proton.me>
2025-12-01 09:27:58 +0100

    rm pkg

--- home-manager/pkgs-gui.nix
+++ home-manager/pkgs-gui.nix
@@ -33,7 +33,6 @@
       # protonvpn-cli # sadly, does not let me log in (API error)
       # and gui doesn't remember credentials after logout/login (works after reboot again), even with those 3 pkgs below 
       protonvpn-gui
-      python313Packages.protonvpn-nm-lib # ProtonVPN NetworkManager Library intended for every ProtonVPN service user
       python313Packages.proton-keyring-linux # ProtonVPN core component to access Linux's keyring
       python313Packages.proton-vpn-network-manager # Provides the necessary functionality for other ProtonVPN components to interact with NetworkManager
     ];
