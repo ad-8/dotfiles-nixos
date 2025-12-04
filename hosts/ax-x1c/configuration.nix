776c06b6eaf3627d63b46d2c0aad4d0313ff5754
arthur <arthurfleck8@proton.me>
2025-12-04 19:07:12 +0100

    this should be all ...

--- hosts/ax-x1c/configuration.nix
+++ hosts/ax-x1c/configuration.nix
@@ -18,7 +18,7 @@
 
 
     boot.initrd.luks.devices."luks-f43e8971-1fb7-4d8c-be86-c8162a78d104".device = "/dev/disk/by-uuid/f43e8971-1fb7-4d8c-be86-c8162a78d104";
-    networking.hostName = "ax-mac"; # Define your hostname.
+    networking.hostName = "ax-x1c"; # Define your hostname.
 
     # ignore short presses of the power button entirely. Long-pressing your power button (5 seconds or longer)
     # to do a hard reset is handled by your machine’s BIOS/EFI and thus still possible.
