a9f64d7409f7911a857bb9c76278482611f47d6c
arthur <arthurfleck8@proton.me>
2025-12-04 19:05:03 +0100

    copy ax-mac to ax-x1c

---
+++ hosts/ax-x1c/configuration.nix
@@ -0,0 +1,57 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ../configuration-core.nix
+    ../ax-configs.nix
+    ];
+
+    configExtra.enable = true;
+    configPrinting.enable = true;
+    configVirtman.enable = false;
+
+
+    boot.initrd.luks.devices."luks-f43e8971-1fb7-4d8c-be86-c8162a78d104".device = "/dev/disk/by-uuid/f43e8971-1fb7-4d8c-be86-c8162a78d104";
+    networking.hostName = "ax-mac"; # Define your hostname.
+
+    # ignore short presses of the power button entirely. Long-pressing your power button (5 seconds or longer)
+    # to do a hard reset is handled by your machine’s BIOS/EFI and thus still possible.
+    # (https://wiki.nixos.org/wiki/Systemd/logind)
+    services.logind.settings.Login.HandlePowerKey = "ignore"; # started working after a reboot
+
+    services.keyd = {
+      enable = true;
+      keyboards = {
+        default = {
+          ids = [ "*" ];
+          settings = {
+            main = {
+              # Maps capslock to escape when pressed and control when held.
+              capslock = "overload(control, esc)";
+              # Remaps the escape key to capslock
+              # esc = "capslock";
+            };
+          };
+        };
+      };
+    };
+
+    # laptop battery info via `upower --dump`
+    services.upower.enable = true;
+
+
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "25.05"; # Did you read the comment?
+}
+
---
+++ hosts/ax-x1c/home.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ../../home-manager/all-modules.nix
+  ];
+
+  # https://github.com/vimjoyer/modularize-video (as seen in https://www.youtube.com/watch?v=vYc6IzKvAJQ)
+  # pkgsCore.enable = true; # enabled by default, see all-modules.nix
+  pkgsDev.enable = true;
+  pkgsExtra.enable = true;
+  pkgsGui.enable = true;
+  pkgsWm.enable = true;
+
+  configAuthAgent.enable = true;
+  configGit.enable = true;
+  configGtk.enable = true;
+  configMegasync.enable = false;
+  configMime.enable = true;
+  configQt.enable = true;
+  configSecretService.enable = true;
+  configWebApps.enable = true;
+
+  home.stateVersion = "25.05";
+  home.username = "ax";
+  home.homeDirectory = "/home/ax";
+  home.packages = with pkgs; [
+  ];
+
+  programs.bash = {
+	  enable = true;
+    shellAliases = {
+      btw = "echo 'home-manager seems to be working :)'";
+    };
+  };
+}
+
