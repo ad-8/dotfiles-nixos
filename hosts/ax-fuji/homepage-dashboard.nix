6a69d3b956147754f2c403fd24bf03c1ead5849e
arthur <arthurfleck8@proton.me>
2026-01-21 19:28:23 +0100

    add some services

--- hosts/ax-fuji/homepage-dashboard.nix
+++ hosts/ax-fuji/homepage-dashboard.nix
@@ -38,8 +38,40 @@
         };
       }
     ];
-    services = {
+    services = [
+      {
+        "Media" = [
+          {
+            Immich = {
+              href = "http://192.168.178.8:2283";
+              icon = "immich.png";
+              description = "Photos";
+              siteMonitor = "http://192.168.178.8:2283";
+            };
+          }
+          {
+            Jellyfin = {
+              href = "http://192.168.178.8:8096";
+              icon = "jellyfin.png";
+              description = "Media Server";
+              siteMonitor = "http://192.168.178.8:8096";
+            };
+          }
+        ];
+      }
+      {
+        "Misc" = [
+          {
+            Linkding = {
+              href = "http://192.168.178.8:9090";
+              icon = "linkding.png";
+              description = "Bookmarks";
+              siteMonitor = "http://192.168.178.8:9090";
+            };
+          }
+        ];
+      }
+    ];
 
-    };
   };
 }
